7bc447e2be80faaaf4982746d8c65c58abf5dec9
Lucas Caminha <lcaminha@live.com>
2024-01-03 15:52:10 -0300

    Adicionando conteúdo da unidade 1.2.1

---
+++ projects/p1-jogos/multiplicacao.py
@@ -0,0 +1,10 @@
+
+numero1 = 10
+numero2 = "20"
+produto = numero1 * numero2
+
+print(produto)
+# Resultado: 20202020202020202020
+
+print(numero1 * int(numero2))
+# Resultado: 200
---
+++ projects/p1-jogos/teste_soma.py
@@ -0,0 +1,7 @@
+
+idade1 = 10
+#idade2 = "20"#erro
+idade2 = int("20")
+
+print(idade1 + idade2)
+
