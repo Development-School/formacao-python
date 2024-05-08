bbd2411f75485b2874d58a6e1819c730cde887ce
Lucas Caminha <lcaminha@live.com>
2024-05-08 15:04:25 -0300

    Adicionando conteúdo da unidade 2.4.1

---
+++ unit_2/mod_4/extrator-url/main.py
@@ -0,0 +1,31 @@
+
+
+url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
+print(url)
+
+texto = 'abcde'
+print(texto[0])
+# 'a'
+
+print(texto[0:1])
+# 'a'
+
+print(texto[0:2])
+# 'ab'
+
+url = "bytebank.com/cambio?moedaOrigem=real"
+print(url)
+
+url_base = url[0:19]
+print(url_base)
+
+url = "bytebank.com/cambio?moedaOrigem=real"
+print(url)
+
+url_base = url[0:19]
+print(url_base)
+
+url_parametros = url[20:36]
+print(url_parametros)
+
+# bytebank.com/cambio?moedaOrigem=real
---
+++ unit_2/mod_4/fatiando_nomes.py
@@ -0,0 +1,9 @@
+
+
+nome_completo = "Gabriel Saldanha"
+
+primeiro_nome = nome_completo[0:7]
+ultimo_nome = nome_completo[8:16]
+
+print('\n' + primeiro_nome)
+print(ultimo_nome)
